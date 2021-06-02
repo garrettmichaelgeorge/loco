c015b4247e96887c5e5beb4769f949c4d6b7c707
Garrett George <garrett.michael.george@gmail.com>
2021-06-01 21:15:57 -0400

    Add Credo

---
+++ .credo.exs
@@ -0,0 +1,17 @@
+%{
+  configs: [
+    %{
+      name: "default",
+      files: %{
+        included: ["lib/"],
+        excluded: []
+      },
+      plugins: [],
+      requires: [],
+      strict: false,
+      parse_timeout: 5000,
+      color: true,
+      checks: []
+    }
+  ]
+}
